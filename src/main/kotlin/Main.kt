99cac47a91d62b60842b2950e43d09e3e38ea7f8
Bruends <brunomg962@gmail.com>
2023-06-04 20:42:13 -0300

    aula 1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("hello world")
+    val titular = "Bruno"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo += 100.00
+
+    println("titular: $titular")
+    println("numero da conta: $numeroConta")
+    println("saldo: $saldo")
+    
+}
